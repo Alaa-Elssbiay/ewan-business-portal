27f24f607f692e2f9928caf710d7d73a5b768593
Alaa Elsbeay <dev6@ewanintl.sa>
2025-09-22 09:49:29 +0300

    update footer

--- src/app/layout/app.layout.module.ts
+++ src/app/layout/app.layout.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import {
+    provideHttpClient,
+    withInterceptorsFromDi,
+} from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { InputTextModule } from 'primeng/inputtext';
 import { SidebarModule } from 'primeng/sidebar';
@@ -13,20 +16,21 @@ import { AppMenuComponent } from './app.menu.component';
 import { AppMenuitemComponent } from './app.menuitem.component';
 import { RouterModule } from '@angular/router';
 import { AppTopBarComponent } from './app.topbar.component';
-import { AppFooterComponent } from './app.footer.component';
-import { AppSidebarComponent } from "./app.sidebar.component";
-import { AppLayoutComponent } from "./app.layout.component";
+import { AppSidebarComponent } from './app.sidebar.component';
+import { AppLayoutComponent } from './app.layout.component';
 import { AppConfigModule } from './config/config.module';
 
-@NgModule({ declarations: [
+@NgModule({
+    declarations: [
         AppMenuitemComponent,
         AppTopBarComponent,
-        AppFooterComponent,
         AppMenuComponent,
         AppSidebarComponent,
         AppLayoutComponent,
     ],
-    exports: [AppLayoutComponent], imports: [BrowserModule,
+    exports: [AppLayoutComponent],
+    imports: [
+        BrowserModule,
         FormsModule,
         BrowserAnimationsModule,
         InputTextModule,
@@ -36,5 +40,8 @@ import { AppConfigModule } from './config/config.module';
         InputSwitchModule,
         RippleModule,
         RouterModule,
-        AppConfigModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
-export class AppLayoutModule { }
+        AppConfigModule,
+    ],
+    providers: [provideHttpClient(withInterceptorsFromDi())],
+})
+export class AppLayoutModule {}
